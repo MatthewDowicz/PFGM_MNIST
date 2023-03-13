f06f83c22f907006a46a5278e6759877242a07aa
Matthew James Dowicz <mdowicz@login33.chn>
2023-03-12 20:51:41 -0700

    Added refactored/updated code for more general io and usability with perturbed data.

--- make_dataset.py
+++ make_dataset.py
@@ -4,6 +4,10 @@ import torch.utils.data as data
 from torchvision import transforms
 from torchvision.datasets import MNIST, CIFAR10
 import os
+import jax
+import jax.numpy as jnp
+from jax import random
+import pickle
 
 from typing import Any, Sequence, Optional, Tuple, Iterator, Dict, Callable, Union
 
@@ -14,6 +18,74 @@ def custom_transform(img):
 
 def cifar_transform(img):
     return np.array(img, dtype=np.float32) / 255.
+
+def save_data(data: Any, directory: str, filename: str):
+    """
+    Function to save the 'data' you want to save, to the 'directory' you want,
+    with the 'filename' you want to save it as.
+    
+    Args:
+    -----
+        data: Any
+            The data you want to save e.g. CSV, np.ndarray, etc.
+        directory: str
+            The string representing the path to the directory where 
+            you want to save the data.
+            
+            For example, 'directory = "./data/train"' would specify
+            that you want to save the data in the 'train' directory
+            within the current working directory.
+        filename: str
+            The string that specifies the name of the file to be saved
+            
+    Returns:
+    --------
+        The saved data to the specified directory.
+    """
+    # Check if the directory eexists, and create it if it doesn't
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    filepath = os.path.join(directory, filename)
+    # Save the data into the specified file in the directory
+    if type(data) == np.ndarray: # use 'np.save' if data is an numpy array
+        np.save(filepath, data)
+    else:
+        # Open a file to save the data
+        with open(filepath, 'wb') as f:
+            pickle.dump(data, f)
+            
+def load_data(data_dir: str, data_file: str):
+    """
+    Loads data from a specified file in a specified directory.
+    If the directory doesn't exist, raises an informative error.
+    If the data is a numpy array, uses np.load for optimal loading.
+
+    Parameters:
+    ------------
+        data_dir: str
+            Path to directory containing data file
+        data_file: str
+            Name of data file to load
+
+    Returns:
+    --------
+        data: loaded data
+    """
+
+    # Check if directory exists
+    if not os.path.isdir(data_dir):
+        raise ValueError(f"Directory {data_dir} does not exist.")
+
+    # Load data using optimal method for numpy arrays
+    data_path = os.path.join(data_dir, data_file)
+    if data_file.endswith('.npy') or data_file.endswith('.npz'):
+        data = np.load(data_path)
+    else:
+        with open(data_path, 'rb') as f:
+            data = pickle.load(f)
+
+    return data
 
 def download_MNIST(root_dir: str = 'saved_data/',
                    download: bool = True,
@@ -124,7 +196,26 @@ def partition_MNIST(root_dir: str = 'saved_data/',
         return train_set, val_set, test_dataset
     
     else:
-        train_set, test_dataset
+        return train_set, test_dataset
+
+def reshape_with_channel_dim(arr: np.ndarray):
+    """
+    Helper function to add a channel dimension to the MNIST dataset.
+    It takes the input array ('arr') that we want to add the new dimension to
+    
+    Args:
+    -----
+        arr: np.ndarray
+            The data array we want to add the channel dimension to
+            
+    Returns:
+    --------
+        Returns the reshaped data with a new channel dimension at axis 1
+    """
+    n_samples = arr.shape[0]
+    original_shape = arr.shape[1:]
+    new_shape = (n_samples, 1) + original_shape
+    return np.reshape(arr, new_shape)
 
 
 def numpy_collate(batch: Any):
@@ -400,6 +491,98 @@ def perturb(samples_batch: np.ndarray,
                                             z[:, None]), axis=1)
     return perturbed_samples_vec
 
+def jax_perturb(samples_batch: np.ndarray,
+            prng: Any,
+            sigma: float = 0.05,
+            tau: float = 0.05,
+            M: int = 291,
+            restrict_M: bool = True):
+    """
+    Perturbing the augmented training data. See algorithm 2 in the PFGM paper
+    (https://arxiv.org/pdf/2209.11178.pdf.). Found under models/utils_poisson.py
+    on Github.
+    
+    Args:
+    -----
+        samples_batch: np.ndarray
+            A batch of un-augmented training data.
+        rng: np.random._generator.Generator
+            rng needed for sampling the necessary hyperparameters.
+        sigma: float
+            Noise parameter. Specifically, it's the standard deviation of the 
+            gaussian distribution that is sampled from to get the noise in x/y
+            (eps_x/eps_y).
+        tau: float
+            Hyperparameter. Not sure what it really is.. :)
+        M: float
+            Measure how far out you go from the distribution. 
+            Used to sample m, which is the exponent of (1 + \tau).
+        restrict_M: bool
+            Flag to allow confing the norms of the data to be....
+
+    Returns:
+    --------
+        perturbed_samples_vec: np.ndarray
+            The perturbed samples.
+    """
+    # Splitting of keys to allow for PRNG.
+    # rng, subkey1, subkey2, subkey3, subkey4, subkey5, subkey6 = jax.random.split(prng, num=7)
+    
+    # Sample the exponents of (1+tau) from m ~ U[0,M], should be 1D
+    # m = random.uniform(subkey2, shape=(len(samples_batch),1), minval=0, maxval=M)
+    m = random.uniform(prng, shape=(len(samples_batch),1), minval=0, maxval=M)
+
+    
+    # Sample the noise parameter for the perturbed augmented data, z
+    # Multiplying by sigma changes the variance of the gaussian but not the mean,
+    # which is what Algorithm 2 dictates.                       
+    # eps_z = 0 + sigma * random.normal(subkey3, shape=(len(samples_batch), 1))
+    eps_z = 0 + sigma * random.normal(prng, shape=(len(samples_batch), 1))
+    eps_z = jnp.abs(eps_z)
+
+    # Confine the norms of perturbed data.
+    # See Appendix B.1.1 of paper
+    if restrict_M:
+        idx = jnp.squeeze(eps_z < 0.005)
+        # num = int(jnp.sum(idx))
+        num = jnp.array(jnp.sum(idx), int) # rewrite this as `x.astype(int)` or `jnp.array(x, int)` to work
+        # restrict_m = int(M* 0.7)
+        restrict_m = jnp.array(M*0.7, int)
+
+        # m = m.at[idx, 0].set(random.uniform(subkey4, shape=(num,), minval=0, maxval=restrict_m))
+        m = m.at[idx, 0].set(random.uniform(prng, shape=(num,), minval=0, maxval=restrict_m))
+
+    # data_dim = data_channels (1) * data_size (28) * data_size (28)
+    data_dim = samples_batch.shape[1] * samples_batch.shape[2] * samples_batch.shape[3]
+    factor = (1 + tau) ** m
+
+    # Create the noise parameter for the perturbed data, x.
+    # eps_x = rng.standard_normal(size=(len(samples_batch), data_dim)) * sigma
+    # norm_eps_x = np.linalg.norm(eps_x, ord=2, axis=1) * factor                  
+    # eps_x = 0 + sigma * random.normal(subkey5, shape=(len(samples_batch), data_dim))
+    eps_x = 0 + sigma * random.normal(prng, shape=(len(samples_batch), data_dim))
+
+    norm_eps_x = jnp.linalg.norm(eps_x, ord=2, axis=1) * factor[:, 0]
+                             
+    # Perturb z 
+    z = jnp.squeeze(eps_z) * factor[:,0]
+
+    # Sample uniform angle over unit sphere (u in Eqn. 5)
+    # gaussian = random.normal(subkey6, shape=(len(samples_batch), data_dim))
+    gaussian = random.normal(prng, shape=(len(samples_batch), data_dim))
+    unit_gaussian = gaussian / jnp.linalg.norm(gaussian, ord=2, axis=1, keepdims=True)
+
+    # Construct the perturbation for x
+    perturbation_x = unit_gaussian * norm_eps_x[:, None]
+    perturbation_x = jnp.reshape(perturbation_x, newshape=(samples_batch.shape))
+    # Perturb x (ie. y in Algorithm 2)
+    y = samples_batch + perturbation_x
+
+    # Augment the data with extra dimension z
+    perturbed_samples_vec = np.concatenate((y.reshape(len(samples_batch), -1), 
+                                            z[:,None]), axis=1)
+    return perturbed_samples_vec
+
 
 def get_perturbed_img(perturbed_data: np.ndarray, input_data_shape: Tuple):
     """
@@ -547,3 +730,124 @@ def empirical_field(batch: np.ndarray, rng: Any):
 
     target = gt_direction
     return perturbed_samples_vec, target
+
+def jax_empirical_field(batch: np.ndarray, 
+                        prng: Any, 
+                        sigma: float = 0.05,
+                        tau: float = 0.05,
+                        M: int = 291,
+                        restrict_M: bool = True):
+    """
+    Function to calculate the empirical (ie. seen) Poisson field.
+    This function does the brute force calculation of what the field 
+    looks like and its output are the "labels" for our supervised learning
+    problem. This is the answer that we want the NN to learn to emulate.
+    Found in losses.py under function 'get_loss_fn'
+
+    Args:
+    -----
+        batch: np.ndarray
+            Batch of unperturbed sample data.
+        rng: np.random.generator_Generator
+            rng used for calculating the perturbing hyperparameters (tau/m,sigma)
+        
+    Returns:
+    --------
+        target: np.ndarray
+            Array containing the empirical field for every pixel (ie. data point).
+    """
+    # Create unperturbed batch data vec with an un-augmented extra dimension
+    # (i.e. append an extra dimension to the pixel data with the last dimension being 0)
+    z = jnp.zeros(len(batch))
+    # z[:, None] to create a 2D array with nothing in the 2nd dim b/c it's about to be concatenated
+    unperturbed_samples_vec = jnp.concatenate((batch.reshape(len(batch), -1), 
+                                            z[:, None]), axis=1)
+
+    # Perturb the (augmented) batch data
+    perturbed_samples_vec = jax_perturb(samples_batch=batch,
+                                        prng=prng,
+                                        sigma=sigma,
+                                        tau=tau,
+                                        M=M,
+                                        restrict_M=restrict_M)
+    # batch.shape[1/2] = img_size, batch.shape[3] = n_channels
+    data_dim = batch.shape[1] * batch.shape[2] * batch.shape[3]
+
+    # Get distance between the unperturbed vector on hyperplane (z=0) and their perturbed versions
+    # Expand dims here, so that the 2nd dim of the array doesn't collapse
+    # ie. make sure that gt_distance.shape = (batchsize, batchsize), which corresponds to a vector
+    # in the N+1 dimension space per sample <-- MAKE THIS CLEARER
+    gt_distance = jnp.sqrt(jnp.sum((jnp.expand_dims(perturbed_samples_vec, axis=1) - unperturbed_samples_vec) ** 2,
+                                    axis=-1, keepdims=False))
+
+    # For numerical stability, we multiply each row by its minimum value
+    # keepdims=True, so we don't lose a dimension
+    # Figure out why my code doesn't need a [0] in the numerator of the first distance var
+    distance = jnp.min(gt_distance, axis=1, keepdims=True) / (gt_distance + 1e-7)
+    distance = distance ** (data_dim + 1)
+    distance = distance[:, :, None]
+
+
+    # Normalize the coefficients (effectively multiply by c(x_tilde))
+    # Expand dims again to avoid losing a dimension
+    coeff = distance / (jnp.sum(distance, axis=1, keepdims=True) + 1e-7)
+    diff = - ((jnp.expand_dims(perturbed_samples_vec, axis=1) - unperturbed_samples_vec))
+
+    # Calculate the empirical Poisson Field (N+1 dimension in the augmented space)
+    gt_direction = jnp.sum(coeff * diff, axis=1, keepdims=False)
+    gt_norm = jnp.linalg.norm(gt_direction, axis=1)
+    # Normalize 
+    gt_direction /= jnp.reshape(gt_norm, (-1,1))
+    gt_direction *= jnp.sqrt(data_dim)
+
+    target = np.asarray(gt_direction)
+    return perturbed_samples_vec, target
+
+def process_perturbed_data(dataset: np.ndarray, prng: jax.random.PRNGKey,
+                           sigma: float = 0.05,
+                            tau: float = 0.05,
+                            M: int = 291,
+                            restrict_M: bool = True):
+    """
+    Function that perturbs the raw MNIST files (ie. train data file/test data file)
+    and saves them into a tuple of (perturbed_data, empirical_field).
+    
+    It does this by calculating the number of passes needed to go through the entire
+    dataset if we used a batchsize of 1000. The reason for the seemingly arbitrary choice
+    of 1000, is that I'm running out of memory at when my batchsize is larger than 1000.
+    
+    Args:
+    -----
+        dataset: np.ndarray
+            Either the raw MNIST training/testing dataset.
+            NOTE: Dataset here refers to the pixel data only, we discard the labels because
+                  in PFGM the NN learns the empirical field at each pixel location. One can
+                  think of the empirical field at each pixel location as that pixels "label".
+                  
+        prng: jax.random.PRNGKey
+            Source of randomness for generating random numbers  
+            
+    Returns:
+    --------
+        tuple of the perturbed data for each sample and the empirical field for each image
+    """
+    # Get the number of samples in the dataset
+    num_samples = dataset.shape[0]
+    # Calculate the number of passes needed to go through the entire dataset
+    # in batches of 1000
+    num_passes = int(np.ceil(num_samples / 1000))
+    # Initialize empty list to store the output of the function
+    outputs = []
+    # Loop over the number passes needed to go through entire dataset
+    for i in range(num_passes):
+        # Calculate start and end indices for the current batch
+        start_idx = i * 1000
+        end_idx = min((i+1)*1000, num_samples)
+        # Get the current batch from the dataset
+        batch = dataset[start_idx:end_idx]
+        # Get the perturbed data & empirical field for that batch
+        output = jax_empirical_field(batch, prng, sigma=sigma, tau=tau, M=M, restrict_M=restrict_M)
+        outputs.append(output)
+        del output
+    # Concatenate outputs along the first axis
+    return tuple(np.concatenate(o, axis=0) for o in zip(*outputs))
